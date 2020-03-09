cf18e483caa801e3c19ea8c9f8c5549b82083bf1
charliePaguay <charlie.paguay@gmail.com>
2020-03-09 17:07:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pdyzlx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pdyzlx)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+//import { HelloComponent } from './hello.component'; 
+import {HeaderComponent} from './header/HeaderComponent';
+
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent,HelloComponent,HeaderComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
